11ff958d4510935d0167cee147ea407a489ac08f
skytot <skytot@163.com>
2025-06-23 11:07:20 +0800

    Update checkin.js

--- workflows/checkin.js
+++ workflows/checkin.js
@@ -255,11 +255,11 @@ class CheckIn {
     console.log(`运行 ${this.growthTask.taskName}`);
     await this.growthTask.run();
     console.log(`运行 ${this.dipLuckyTask.taskName}`);
-    await this.dipLuckyTask.run();
+    // await this.dipLuckyTask.run();
     console.log(`运行 ${this.lotteriesTask.taskName}`);
     await this.lotteriesTask.run(this.growthTask, this.dipLuckyTask);
     console.log(`运行 ${this.bugfixTask.taskName}`);
-    await this.bugfixTask.run();
+    // await this.bugfixTask.run();
     await juejin.logout();
     console.log("-------------------------");
 
