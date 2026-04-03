f761a8280e8b8af6cebb610beecbab8024e47e4d
skytot <skytot@163.com>
2026-04-03 10:51:55 +0800

    Update env.js

--- workflows/utils/env.js
+++ workflows/utils/env.js
@@ -3,6 +3,8 @@ const env = process.env || {};
 module.exports = {
   /* 掘金Cookie */
   COOKIE: env.COOKIE,
+  /* GLADNS Cookie */
+  GKEY: env.GKEY,
   /* 多用户掘金Cookie, 当有1名以上用户时填写, 支持同时最多可配置5名用户 */
   COOKIE_2: env.COOKIE_2,
   COOKIE_3: env.COOKIE_3,
